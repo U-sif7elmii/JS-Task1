f1dc581dbe4db073fbe1a8ffcc508f50e7e460c1
U-sif7elmii <helmyyousef90@gmail.com>
2025-10-14 17:33:25 +0300

    Section2 'About' End

--- JS/main.js
+++ JS/main.js
@@ -1,3 +1,4 @@
+// Home Page
 document.getElementById("navItem1").innerHTML="About Us" 
 document.getElementById("navItem2").innerHTML="Our Products" 
 document.getElementById("navItem3").innerHTML="We Supply" 
@@ -6,3 +7,13 @@ document.getElementById("homeContent1").innerHTML="BUILDING MATERIALS"
 document.getElementById("homeContent2").innerHTML=`Building success, <br>
 one material, <br>
 at a time` 
+
+
+// About Page
+document.getElementById("aboutContent1").innerHTML="About Us"
+document.getElementById("aboutContent2").innerHTML="What We Do?"
+document.getElementById("aboutContent3").innerHTML="We are specialized in sourcing and providing high-quality building materials to meet the diverse needs of construction projects, ensuring excellence in every structure we contribute to."
+document.getElementById("aboutContent4").innerHTML="What we offer?"
+document.getElementById("aboutContent5").innerHTML="We offer a comprehensive range of top-notch building materials, providing clients with a one-stop solution for their construction needs."
+document.getElementById("aboutContent6").innerHTML="Our Services"
+document.getElementById("aboutContent7").innerHTML="Your one-stop solution for hassle-free project material procurement, providing top-quality materials and expert guidance."
