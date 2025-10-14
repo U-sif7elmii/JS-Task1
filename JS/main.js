25e7abbc3d6785e27c58d71c5a7af7ab7d72c552
U-sif7elmii <helmyyousef90@gmail.com>
2025-10-14 12:46:05 +0300

    Section1 'Home' End

---
+++ JS/main.js
@@ -0,0 +1,8 @@
+document.getElementById("navItem1").innerHTML="About Us" 
+document.getElementById("navItem2").innerHTML="Our Products" 
+document.getElementById("navItem3").innerHTML="We Supply" 
+document.getElementById("navItem4").innerHTML="Contact Us" 
+document.getElementById("homeContent1").innerHTML="BUILDING MATERIALS" 
+document.getElementById("homeContent2").innerHTML=`Building success, <br>
+one material, <br>
+at a time` 
